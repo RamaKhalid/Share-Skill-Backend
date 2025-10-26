fe611aae3264958fca2329a52e5378e0eb339d6b
Rama <RamaKhalidCS@outlook.com>
2025-10-26 23:19:59 +0300

    Create meeting models

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
 #
@@ -14,3 +15,9 @@ class Experience (models.Model):
     date= models.DateField ('Experience data')
     description= models.CharField (max_length=100)
     place= models.CharField (max_length=100)
+
+class Meeting (models.Model):
+    date= models.DateField ('Meeting data')
+    time= models.TimeField ('Meeting data')
+    is_complete= models.BooleanField ()
+    rate= models.PositiveIntegerField (validators= [MaxValueValidator(5)])
