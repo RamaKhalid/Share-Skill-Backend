a63825ae29860bb29d5a6719422c7962d3a6797a
Rama <RamaKhalidCS@outlook.com>
2025-10-26 23:33:22 +0300

    Create Certificate models and update Experience model

--- main_app/models.py
+++ main_app/models.py
@@ -13,11 +13,23 @@ class Skill (models.Model):
 
 class Experience (models.Model):
     date= models.DateField ('Experience data')
+    title = models.CharField (max_length=100)
     description= models.CharField (max_length=100)
     place= models.CharField (max_length=100)
+
 
 class Meeting (models.Model):
     date= models.DateField ('Meeting data')
     time= models.TimeField ('Meeting data')
     is_complete= models.BooleanField ()
     rate= models.PositiveIntegerField (validators= [MaxValueValidator(5)])
+
+    def __str__(self):
+        return f'Metting on:{self.date } - {self.time}'
+
+class Certificate (models.Model):
+    type= models.CharField (max_length=100)
+    name= models.CharField (max_length=100)
+
+    def __str__(self):
+        return self.name
