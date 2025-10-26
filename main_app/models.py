aacb7c0484c01abd730d8b1be37ee48e90941c11
Rama <RamaKhalidCS@outlook.com>
2025-10-26 23:12:54 +0300

    Create Skill and Experience models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+#
+class Skill (models.Model):
+    type= models.CharField (max_length=100)
+    name= models.CharField (max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+
+class Experience (models.Model):
+    date= models.DateField ('Experience data')
+    description= models.CharField (max_length=100)
+    place= models.CharField (max_length=100)
--- share_skill_project/settings.py
+++ share_skill_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app_name'
+    'main_app'
 ]
 
 MIDDLEWARE = [
