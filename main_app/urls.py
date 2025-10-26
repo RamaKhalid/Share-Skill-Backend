94231480ad85295d4110f7a8ff7f780005102388
Rama <RamaKhalidCS@outlook.com>
2025-10-27 00:05:34 +0300

    set files

---
+++ main_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import *
+# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns= [
+    
+]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
